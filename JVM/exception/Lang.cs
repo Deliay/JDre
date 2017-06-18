bfc4a9428ff6589fb7aacaa1f65474cfc31ce5ab
Deliay <admin@remiliascarlet.com>
2017-06-18 18:31:50 +0800

    Move and add  some Excpetion to exception namespace

---
+++ JVM/exception/Lang.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDRE.JVM.exception.Lang
+{
+    class ClassFormatError : Exception
+    {
+        public ClassFormatError(string message) : base(message) { }
+    }
+
+    class UnsupportedClassVersionError : Exception { }
+
+    class NullPointerException : Exception { }
+
+    class InstantiationError : Exception { }
+
+    class IllegalAccessError : Exception { }
+
+    class ClassCastException : Exception { }
+
+    class ClsasFormatError : Exception { }
+
+    class IncompatiableClassChangeError : Exception { }
+}
